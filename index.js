c1765587d8c3506b06120f267774ef2c0f89c450
craig johnson <cj.craigajohnson@gmail.com>
2023-05-20 12:48:41 -0400

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+
+}
+
+function destructivelyPrependCat(name) {
+return cats.unshift(name);
+
+}
+
+function destructivelyRemoveLastCat(){
+  return cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+  return cats.shift();
+}
+
+function appendCat(name) {
+  const appendCat = [...cats, name]
+  return appendCat
+}
+
+function prependCat(name) {
+ return [name, ...cats]
+}
+
+function removeLastCat() {
+  return cats.slice(0,2);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
